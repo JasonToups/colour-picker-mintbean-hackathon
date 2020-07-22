f106b66f12390c66ee709fbcab575551312481a0
jason-toups <me@jasontoups.com>
2020-07-22 12:49:17 -0700

    layout adjustments

--- src/Colors.jsx
+++ src/Colors.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
-function copyColor(e) {
-	console.log(e);
-}
+// function copyColor(e) {
+// 	console.log(e);
+// }
 
 export default function Colors({ hex }) {
 	return (
-		<div
-			style={{ backgroundColor: hex }}
-			className='color'
-			onClick={copyColor(hex)}>
+		<div style={{ backgroundColor: hex }} className='color'>
 			<div className='hex-value'>{hex}</div>
 		</div>
 	);
