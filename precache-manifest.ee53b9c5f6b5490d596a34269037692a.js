09724e71925a4a94e23e19c5da4ed3215a8c32c9
jason-toups <me@jasontoups.com>
2020-07-22 10:31:53 -0700

    Updates

---
+++ precache-manifest.ee53b9c5f6b5490d596a34269037692a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962a6fdaa885f49ac2aec3372013473c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc945d370f82a8f05a72",
+    "url": "/static/css/main.81bbacd0.chunk.css"
+  },
+  {
+    "revision": "07e00e82b775e5f6f5a7",
+    "url": "/static/js/2.f7ff3ccd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f7ff3ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc945d370f82a8f05a72",
+    "url": "/static/js/main.bffc1403.chunk.js"
+  },
+  {
+    "revision": "5d575cb8a35894044216",
+    "url": "/static/js/runtime-main.c580e89e.js"
+  }
+]);
